b2a63fb54b1dfd976db09cbf2ac8e13ad093b448
jyong <jyong7851@sina.com>
2019-04-06 17:56:13 +0800

    2019.4.6

--- python/kafka/src/calc.py
+++ python/kafka/src/calc.py
@@ -1,11 +1,21 @@
-import zerorpc
-
+import zerorpc,json
+from kazoo.client import KazooClient
 class HelloRPC(object):
     i = 0
     def hello(self, name):
         self.i = self.i+  int(name)
         print(self.i)
         return "Hello, %s" % name
+
+zk = KazooClient(hosts="192.168.1.12:2181")
+zk.start()
+value = json.dumps({'host': '192.168.1.10', 'port': '4242'})
+zk.ensure_path('/socket_server/zeropc_server')
+if not zk.exists('/socket_server/zeropc_server/port'):
+   zk.create('/socket_server/zeropc_server/port', value.encode(),ephemeral=True)
+print(zk.exists("/socket_server/zeropc_server"))
+print(zk.get("/socket_server/zeropc_server/port"))
+
 
 
 s = zerorpc.Server(HelloRPC())
